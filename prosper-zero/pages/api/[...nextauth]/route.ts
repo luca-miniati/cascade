b47788ef2c8245b6fd9816f3e0c0f5fe6fc97bc2
loo <luca.miniati@gmail.com>
2023-11-12 13:39:11 -0500

    update layout

---
+++ prosper-zero/pages/api/[...nextauth]/route.ts
@@ -0,0 +1,15 @@
+// import NextAuth from "next-auth";
+//
+// export const authOptions = {
+//   providers: 
+//     {
+//       id: "prosper",
+//       name: "Prosper",
+//       type: "oauth",
+//       scope: "read_user_profile read_prosper_account read_invest_order write_invest_order read_loan read_note read_listing",
+//       authorization: "https://www.prosper.com/oauth",
+//       token: "https://api.prosper.com/v1/security/oauth/token",
+//     },
+// };
+//
+// export default NextAuth(authOptions);
