eeb3508a19aab6e7c7dddb1657dd54aa044be9eb
loo <luca.miniati@gmail.com>
2024-01-02 14:54:41 -0800

    fix: oauth

---
+++ prosper-zero/app/api/auth/callback/prosper.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+const CallbackPage = () => {
+    const router =  useRouter();
+    
+    useEffect(() => {
+        const searchParams = useSearchParams();
+        const authKey = searchParams.get('auth_key');
+
+        // TODO: check if state is valid
+
+        if (authKey) {
+            console.log('Got Auth Key');
+            router.push('/');
+        }
+
+    }, [router]);
+
+    return (
+        <p>Loading...</p>
+    );
+}
+
+export default CallbackPage;
+
