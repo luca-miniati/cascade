018844ac8b9140339347a2af225c1d77ea7c6ca1
Luca Miniati <luca.miniati@gmail.com>
2024-01-11 15:45:46 -0500

    fix: navbar path

--- prosper-zero/app/signUp/page.tsx
+++ prosper-zero/app/signUp/page.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react';
 import Background from '../components/background';
-import Navbar from '../components/navbar';
+import Navbar from '../components/topNavbar';
 import SignUp from '../components/signUp';
 
 const Home: FC = () => {
     const backgroundImageUrl = '/gradients/02.png';
     return (
         <main className="h-screen text-white">
-            <Background url={backgroundImageUrl}>
-                <Navbar/>
+            <Background flexDirection="column" url={backgroundImageUrl}>
+                <Navbar navbarIndex={-1}/>
                 <SignUp/>
             </Background>
         </main>
