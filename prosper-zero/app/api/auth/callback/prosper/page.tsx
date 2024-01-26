4748671e2650a0bd3315bce857a9c53611ff33bf
loo <luca.miniati@gmail.com>
2024-01-26 16:18:04 -0500

    update backend

--- prosper-zero/app/api/auth/callback/prosper/page.tsx
+++ prosper-zero/app/api/auth/callback/prosper/page.tsx
@@ -2,13 +2,24 @@
 
 import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+// import { createClient } from '@supabase/supabase-js';
+
 
 const CallbackPage = () => {
     const router =  useRouter();
     const searchParams = useSearchParams();
+    // const url = process.env['SUPABASE_URL'];
+    // const key = process.env['SUPABASE_ANON_KEY'];
+    // const supabase = createClient(url, key);
     
     useEffect(() => {
         const authKey = searchParams.get('auth_key');
+        console.log("HERES THE KEYS:");
+        console.log(searchParams.keys());
+
+        // const { error } = await supabase
+        // .from('users')
+        // .insert(user);
 
         // TODO: check if state is valid
 
--- supabase/utils/insertUser.mjs
+++ supabase/utils/insertUser.mjs
@@ -4,11 +4,3 @@ import 'dotenv/config';
 const url = process.env['SUPABASE_URL'] ?? '';
 const key = process.env['SUPABASE_ANON_KEY'] ?? '';
 const supabase = createClient(url, key);
-
-const { data, error } = supabase.from('users').select('*');
-
-if (error) {
-    throw error;
-}
-
-console.log(data);
