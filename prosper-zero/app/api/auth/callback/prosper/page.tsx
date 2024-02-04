7e674ac571100ed41a33fd3f9ba40635225ae4cf
loo <luca.miniati@gmail.com>
2024-02-04 18:03:21 -0500

    Fix Null Checking

--- prosper-zero/app/api/auth/callback/prosper/page.tsx
+++ prosper-zero/app/api/auth/callback/prosper/page.tsx
@@ -9,8 +9,8 @@ const CallbackPage = () => {
     const router =  useRouter();
     const searchParams = useSearchParams();
     console.log(searchParams);
-    const url = process.env['SUPABASE_URL'];
-    const key = process.env['SUPABASE_ANON_KEY'];
+    const url = process.env['SUPABASE_URL'] ?? '';
+    const key = process.env['SUPABASE_ANON_KEY'] ?? '';
     const supabase = createClient(url, key);
     
     useEffect(() => {
