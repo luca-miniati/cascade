3b6ddf7e8669878e36cd1e03f407c99f2fa90abc
Luca Miniati <luca.miniati@gmail.com>
2024-02-04 13:09:25 -0500

    ts model

---
+++ supabase/functions/model/index.ts
@@ -0,0 +1,96 @@
+import { Loan, Listing, OptimizationType, DEFAULT_PROBS } from '../utils/index.ts';
+import GLPK from 'npm:glpk.js';
+
+/*
+    * ProsperZero
+        * FIELDS
+        * this.portfolio : Loan[]
+        * this.riskWeight : number
+        * FUNCTIONS
+        * this.optimize : Listings[], allocation -> Listings[]
+*/
+
+const glpk = GLPK();
+
+export class ProsperZero {
+    portfolio: Loan[];
+    optimizationType: OptimizationType;
+
+    constructor(portfolio: Loan[], optimizationType: OptimizationType) {
+        this.portfolio = portfolio;
+        this.optimizationType = optimizationType;
+
+    }
+
+    getListingValuation(listing: Listing): number {
+        switch (this.optimizationType) {
+            case OptimizationType.MAX_INTEREST: {
+                return (listing.lenderYield -
+                        listing.lenderYield * DEFAULT_PROBS[listing.prosperRating]) * listing.term;
+            }
+
+            case OptimizationType.MAX_CASH_FLOW: {
+                return listing.lenderYield -
+                    listing.lenderYield * DEFAULT_PROBS[listing.prosperRating];
+            }
+
+            default: {
+                return listing.lenderYield -
+                    listing.lenderYield * DEFAULT_PROBS[listing.prosperRating];
+            }
+        }
+    }
+
+    optimize(listings: Listing[], maxAllocation: number) {
+        const numListings: number = listings.length;
+        const listingBinaries: string[] = [];
+        const listingVars: Object[] = [];
+
+        for (let i = 0; i < numListings;  i++) {
+            
+            const id = listings[i].id;
+            const val = this.getListingValuation(listings[i]);
+            listingBinaries.push(id);
+            listingVars.push({
+                name: id,
+                coef: val,
+            });
+        }
+
+        const lp: Object = {
+            name: 'P2P Loan Optimization',
+            objective: {
+                direction: glpk.GLP_MAX,
+                name: 'obj',
+                vars: listingVars,
+            },
+            subjectTo: [{
+                name: 'Allocation Constraint',
+                vars: listingBinaries.map(id => ({ name: id, coef: 1 })),
+                bnds: {
+                    type: glpk.GLP_UP,
+                    ub: maxAllocation / 25,
+                    lb: 0,
+                },
+            }],
+            binaries: listingBinaries,
+        }
+
+        const res: Object = glpk.solve(lp);
+        console.log(res);
+    }
+}
+
+const listings = [
+    new Listing('0', 0.1775, 'C', 4, 'COMPLETED', 10000.0),
+    new Listing('1', 0.2384, 'E', 4, 'COMPLETED', 10000.0),
+    new Listing('2', 0.2254, 'D', 3, 'COMPLETED', 10000.0),
+    new Listing('3', 0.1205, 'B', 3, 'COMPLETED', 10000.0),
+    new Listing('4', 0.2033, 'C', 5, 'COMPLETED', 10000.0),
+    new Listing('5', 0.1511, 'B', 3, 'COMPLETED', 10000.0),
+    new Listing('6', 0.1961, 'C', 3, 'COMPLETED', 10000.0),
+    new Listing('7', 0.1745, 'C', 2, 'COMPLETED', 10000.0),
+];
+
+const model = new ProsperZero([], OptimizationType.MAX_INTEREST);
+model.optimize(listings, 100.0);
---
+++ supabase/functions/utils/index.ts
@@ -0,0 +1,50 @@
+export const DEFAULT_PROBS = {
+    'AA' : 0.059844,
+    'A' :  0.106875,
+    'B' :  0.158608,
+    'C' :  0.230475,
+    'D' :  0.285390,
+    'E' :  0.286738,
+    'HR' : 0.341582,
+}
+
+export enum OptimizationType {
+    DEFAULT,
+    MAX_INTEREST,
+    MAX_CASH_FLOW,
+}
+
+
+export class Loan {
+    lenderYield: number;
+    term: number;
+
+    constructor(lenderYield: number, term: number) {
+        this.lenderYield = lenderYield;
+        this.term = term;
+    }
+}
+
+
+export class Listing {
+    id: string;
+    lenderYield: number;
+    term: number;
+    prosperRating: string;
+    loanStatus: string;
+    principalPaid: number;
+
+    constructor(id: string, lenderYield: number, prosperRating: string, term: number,
+                loanStatus: string, principalPaid: number) {
+        this.id = id;
+        this.lenderYield = lenderYield;
+        this.prosperRating = prosperRating;
+        this.term = term;
+        this.loanStatus = loanStatus;
+        this.principalPaid = principalPaid;
+    }
+
+    isDefaulted() {
+        return this.loanStatus == 'DEFAULTED';
+    }
+}
