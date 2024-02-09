b53bee52ba66faeb941684ccf0fffb954c3413b9
loo <luca.miniati@gmail.com>
2024-02-09 00:29:41 -0500

    im so tired

--- supabase/functions/backtest/investor/index.ts
+++ supabase/functions/backtest/investor/index.ts
@@ -1,5 +1,5 @@
 import { IModel } from '../../model/index.ts';
-import { Loan, OptimizationType, DEFAULT_PROBS } from '../../utils/index.ts';
+import { Listing, Loan, OptimizationType, DEFAULT_PROBS } from '../../utils/index.ts';
 
 /*
     * Investor
@@ -17,23 +17,48 @@ import { Loan, OptimizationType, DEFAULT_PROBS } from '../../utils/index.ts';
 */
 
 export class Investor{
-    portfolio : Loan[];
-    initialInvestment : number;
-    currentCashBalance : number;
-    model : IModel;
-    monthlyAllocation : number;
+    portfolio: Loan[];
+    initialInvestment: number;
+    currentCashBalance: number;
+    model: IModel;
+    monthlyAllocation: number;
+    dailyAllocation: number[];
 
-    constructor(portfolio : Loan[], initialInvestment : number, currentCashBalance : number, 
-        model : IModel, monthlyAllocation : number){
-            this.portfolio = portfolio;
-            this.initialInvestment = initialInvestment;
-            this.currentCashBalance = currentCashBalance;
-            this.model = model;
-            this.monthlyAllocation = monthlyAllocation;
+    constructor(portfolio: Loan[], initialInvestment: number, currentCashBalance: number, model: IModel, monthlyAllocation: number) {
+        this.portfolio = portfolio;
+        this.initialInvestment = initialInvestment;
+        this.currentCashBalance = currentCashBalance;
+        this.model = model;
+        this.monthlyAllocation = monthlyAllocation;
+        this.dailyAllocation = [];
     }
 
-    currentAllocation(currentDay : Date) : number {
-        // based on the monthly allocation, break into increments of 25$ and overflow if more than the days 
+    setAllocation(currentDay: Date): void {
+        const daysInMonth = new Date(currentDay.getFullYear(), currentDay.getMonth() + 1, 0).getDate();
+        let remainingAllocation = this.monthlyAllocation * this.currentCashBalance;
+        const dailyAllocation = Array(daysInMonth).fill(0);
 
+        let day = 0;
+        while (remainingAllocation >= 25) {
+            dailyAllocation[day % daysInMonth] += 1
+            remainingAllocation -= 25;
+            day += 1
+        }
+
+        this.dailyAllocation = dailyAllocation;
+    }
+
+    purchaseListing(listing: Listing): void {
+        this.portfolio.push(
+            listing.toLoan()
+        );
+        this.currentCashBalance -= 25;
+    }
+
+    callModel(listings: Listing[], currentDay: Date): void {
+        const listingsToBuy: Listing[] = this.model.optimize(listings, this.dailyAllocation[currentDay.getDay() - 1]);
+        for (const listing of listingsToBuy) {
+            this.purchaseListing(listing);
+        }
     }
 }
