031023b7e083e0345810134d992468a46ff9a3bf
Luca Miniati <luca.miniati@gmail.com>
2024-02-13 13:09:01 -0500

    update addMonth

--- supabase/functions/utils/index.ts
+++ supabase/functions/utils/index.ts
@@ -24,9 +24,26 @@ export function laterDate(a: Date, b: Date): boolean {
     return a.getTime() > b.getTime();
 }
 
+export function addMonth(a: Date) {
+    const currentMonth = a.getMonth();
+    const newMonth = currentMonth + 1;
+    if (newMonth > 11) {
+        a.setMonth(0);
+        a.setFullYear(a.getFullYear() + 1);
+    } else {
+        a.setMonth(newMonth);
+    }
+    return a;
+}
+
+export function firstOfTheMonth(a: Date): Date {
+    return new Date(a.getFullYear(), a.getMonth(), 1);
+}
+
 abstract class P2P {
     id: string;
     lenderYield: number;
+    monthlyRate: number;
     term: number;
     loanStatus: string;
     principalPaid: number;
@@ -35,71 +52,86 @@ abstract class P2P {
     terminationDate: Date;
     prosperRating: string;
 
-    constructor(id: string, lenderYield: number, term: number, loanStatus:string, principalPaid: number, amountBorrowed: number, originationDate: Date, prosperRating: string) {
-        this.id = id;
-        this.lenderYield = lenderYield;
-        this.term = term;
-        this.loanStatus = loanStatus;
-        this.principalPaid = principalPaid;
-        this.amountBorrowed = amountBorrowed;
-        this.originationDate = originationDate;
-        this.prosperRating = prosperRating;
-        this.terminationDate = this.computeTerminationDate();
-    }
+    constructor(id: string, lenderYield: number, term: number, loanStatus:string,
+                principalPaid: number, amountBorrowed: number, originationDate: Date,
+                prosperRating: string) {
+                    this.id = id;
+                    this.lenderYield = lenderYield;
+                    this.monthlyRate = lenderYield / 12;
+                    this.term = term;
+                    this.loanStatus = loanStatus;
+                    this.principalPaid = principalPaid;
+                    this.amountBorrowed = amountBorrowed;
+                    this.originationDate = originationDate;
+                    this.prosperRating = prosperRating;
+                    this.terminationDate = this.computeTerminationDate();
+                }
 
-    computeTerminationDate(): Date {
-        let numPayments: number;
-        if (this.loanStatus == 'DEFAULTED') {
-            const r = (this.lenderYield + 0.01) / 12;
-            const monthlyPayment = (this.amountBorrowed * r * ((1 + r) ** this.term)) / (((1 + r) ** this.term) - 1);
-            numPayments = Math.ceil(this.principalPaid / monthlyPayment); 
-        } else {
-            numPayments = this.term;
-        }
+                computeMonthlyPayment(): number {
+                    return 25 * (this.monthlyRate * Math.pow(1 + this.monthlyRate, this.term)) /
+                        (Math.pow(1 + this.monthlyRate, this.term) - 1);
+                }
 
-        let terminationMonth = this.originationDate.getMonth() + numPayments;
-        const years = Math.floor(terminationMonth / 12);
-        terminationMonth %= 12;
+                computeTerminationDate(): Date {
+                    let numPayments: number;
+                    if (this.loanStatus == 'DEFAULTED') {
+                        const monthlyPayment = (this.amountBorrowed *
+                                                this.monthlyRate *
+                                                ((1 + this.monthlyRate) ** this.term)) /
+                                                (((1 + this.monthlyRate) ** this.term) - 1);
+                        numPayments = Math.ceil(this.principalPaid / monthlyPayment); 
+                    } else {
+                        numPayments = this.term;
+                    }
 
-        const terminationDate = new Date(this.originationDate);
-        terminationDate.setMonth(terminationMonth)
-        terminationDate.setFullYear(this.originationDate.getFullYear() + years);
+                    let terminationMonth = this.originationDate.getMonth() + numPayments;
+                    const years = Math.floor(terminationMonth / 12);
+                    terminationMonth %= 12;
 
-        return terminationDate;
-    }
+                    const terminationDate = new Date(this.originationDate);
+                    terminationDate.setMonth(terminationMonth)
+                    terminationDate.setFullYear(this.originationDate.getFullYear() + years);
+
+                    return terminationDate;
+                }
 }
 
 export class Loan extends P2P {
     monthlyPayment: number;
-    monthlyPrincipal: number;
-    principalActive: number;
+    principalBalance: number;
     amortizationSchedule: { [key: string]: [number, number] };
 
-    constructor(id: string, lenderYield: number, term: number, loanStatus:string, principalPaid: number, amountBorrowed: number, originationDate: Date, prosperRating: string) {
-        super(id, lenderYield, term, loanStatus, principalPaid, amountBorrowed, originationDate, prosperRating);
+    constructor(id: string, lenderYield: number, term: number, loanStatus:string,
+                principalPaid: number, amountBorrowed: number, originationDate: Date,
+                prosperRating: string) {
+                    super(id, lenderYield, term, loanStatus, principalPaid, amountBorrowed,
+                          originationDate, prosperRating);
 
-        this.monthlyPayment = this.computeMonthlyPayment();
-        this.monthlyPrincipal = this.computeMonthlyPrincipal();
-        this.amortizationSchedule = this.computeAmortizationSchedule();
-        this.principalActive = 25;
-    }
+                          this.monthlyPayment = this.computeMonthlyPayment();
+                          this.amortizationSchedule = this.computeAmortizationSchedule();
+                          this.principalBalance = 25;
+                }
 
-    computeMonthlyPayment(): number {
-        const r = this.lenderYield / 12;
-        return (25 * r * ((1 + r) ** this.term)) / (((1 + r) ** this.term) - 1);
-    }
+                computeAmortizationSchedule(): { [key: string]: [number, number] }  {
+                    let currentDate = firstOfTheMonth(this.originationDate);
 
-    computeMonthlyPrincipal(): number {
-        const r = this.lenderYield / 12;
-        return this.monthlyPayment - (25 * r);
-    }
+                    let principalBalance = 25;
+                    const res: { [key: string]: [number, number] } = {};
 
-    computeAmortizationSchedule(): { [key: string]: [number, number] }  {
-        
-    }
+                    for (let i = 0; i < this.term; i++) {
+                        const interestPayment = parseFloat((principalBalance * this.monthlyRate).toFixed(2));
+                        const principalPayment = parseFloat((this.monthlyPayment - interestPayment).toFixed(2));
+
+                        currentDate = addMonth(currentDate);
+                        res[currentDate.toDateString()] = [interestPayment, principalPayment];
+
+                        principalBalance -= principalPayment;
+                    }
+
+                    return res;
+                }
 }
 
-//
 // const l: Loan = new Loan('0', 0.05, 36, 'DEFAULTED', 5000, 10000, new Date());
 // console.log(l.monthlyPayment);
 // console.log(l.monthlyPrincipal);
@@ -113,20 +145,23 @@ export class Loan extends P2P {
 // console.log('defaulted on: '+l2.terminationDate.toDateString());
 
 export class Listing extends P2P {
-    constructor(id: string, lenderYield: number, term: number, loanStatus: string, principalPaid: number, amountBorrowed: number, originationDate: Date, prosperRating: string) {
-        super(id, lenderYield, term, loanStatus, principalPaid, amountBorrowed, originationDate, prosperRating);
-    }
+    constructor(id: string, lenderYield: number, term: number, loanStatus: string,
+                principalPaid: number, amountBorrowed: number, originationDate: Date,
+                prosperRating: string) {
+                    super(id, lenderYield, term, loanStatus, principalPaid, amountBorrowed, originationDate,
+                          prosperRating);
+                }
 
-    toLoan() {
-        return new Loan(
-            this.id,
-            this.lenderYield,
-            this.term,
-            this.loanStatus,
-            this.principalPaid,
-            this.amountBorrowed,
-            this.originationDate,
-            this.prosperRating,
-        );
-    }
+                toLoan() {
+                    return new Loan(
+                        this.id,
+                        this.lenderYield,
+                        this.term,
+                        this.loanStatus,
+                        this.principalPaid,
+                        this.amountBorrowed,
+                        this.originationDate,
+                        this.prosperRating,
+                    );
+                }
 }
